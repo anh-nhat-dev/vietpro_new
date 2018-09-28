57cb6f9a65cf671bd3b222a08e1e70314b598086
nhatkaka95 <anhnhata8cool@gmail.com>
2018-09-28 14:38:19 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 module.exports = {
 
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "node_blog"
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "bf42f7ac35da0c",
+    password: "db3e5c5a",
+    database: "heroku_e48868d67bdc957",
+    multipleStatements: true
 
 }
